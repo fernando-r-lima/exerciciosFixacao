ec1f0e8b2bde2aa2cdd02d418c6dc906f37bc4b2
Fernando Lima <fernando.ilustrar@gmail.com>
2021-05-24 11:11:10 -0300

    Criado pasta, exerciciosFixacao

---
+++ exerciciosFixacao/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using exerciciosFixacao.exercicioStruct;
+
+namespace exerciciosFixacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            
+            //exercício Struct
+            Point p;                //não precisa de new, pois é um tipo valor...
+            p.X = 10;
+            p.Y = 20;
+            Console.WriteLine(p);
+            p = new Point();        //...mas aceita
+            Console.WriteLine(p);
+            */
+
+            //exercício Matriz
+            double[,] mat;              //declarando uma matriz
+            mat = new double[2, 3];     //instanciando uma matriz de 2 linhas e 3 colunas
+            Console.WriteLine(mat.Length);          //quantos elementos a matriz tem
+            Console.WriteLine(mat.Rank);            //quantas linhas tem
+            Console.WriteLine(mat.GetLength(0));    //tamanho da primeira dimensao da matriz (==Rank)
+            Console.WriteLine(mat.GetLength(1));    //tamanho da segunda dimensao da matriz
+
+        }
+
+
+    }
+}
